d161a0e6cd73ca0a9e1903151594303aa0d1c466
Selita Morgan <65767033+selita-morgan@users.noreply.github.com>
2022-10-15 15:02:33 +0300

    sc

--- util/database.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('Dz4gV8JMuA', 'Dz4gV8JMuA', '2LAttaKAGd', {dialect: 'mysql', host: 'remotemysql.com'});
-
-module.exports = sequelize;
